c8e94a96121da6b271d16812765f2b18cb296699
Magali Pascaud-Topie <magtopie@gmail.com>
2022-08-07 17:05:45 +0200

    création des composants et page Home

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <div>
+            <h2>GROUPOMANIA</h2>
+            <h3>Spécialiste de la grande distribution</h3>
+            <p>Contacter l'administrateur : </p>
+        </div>
+    )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home/Home';
+import Footer from './components/Footer';
+// import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
+ReactDOM.render(
+  <React.StrictMode>
+      <Router>
+        <Footer />
+        <Route path='/'>
+            <App />
+            </Route>   
+        </Router>
+  </React.StrictMode>,
+document.getElementById('root')
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
